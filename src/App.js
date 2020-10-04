d7e73ed5d952cc7277cc327bb0cb334cb18789f3
Mike Leon <michaeljleon@live.com>
2020-10-04 15:03:52 -0700

    Added react to this project.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      This is a react app. Looks like i'm going to learn functional components vs class based
+    </div>
+  );
+}
+
+export default App;
