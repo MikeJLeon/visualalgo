9a2fe82a774bab40a5d6c4fa3ee8b424cc20d8f9
mikejleon <michaeljleon@live.com>
2020-11-08 15:01:13 -0800

    reimplemented steps with new idea of storing it.

--- src/helper/MergeSort.js
+++ src/helper/MergeSort.js
@@ -1,4 +1,4 @@
-function merge(leftArray, rightArray, setArray) {
+function merge(array, leftArray, rightArray, setArray) {
   if (!leftArray || leftArray.length == 0) {
     return rightArray;
   }
@@ -17,8 +17,8 @@ function merge(leftArray, rightArray, setArray) {
       indexLeft++;
     } else {
       newArray.push({
-        "id": leftArray[indexLeft].id,
-        "value": rightArray[indexRight].value,
+        id: leftArray[indexLeft].id,
+        value: rightArray[indexRight].value,
       });
       leftArray[indexLeft].id = rightArray[indexRight].id;
       indexRight++;
@@ -27,7 +27,7 @@ function merge(leftArray, rightArray, setArray) {
   }
   if (leftArray.length > indexLeft) {
     newArray.push(...leftArray.slice(indexLeft));
-  }else{
+  } else {
     newArray.push(...rightArray.slice(indexRight));
   }
   return newArray;
@@ -41,51 +41,45 @@ function MergeSort(array, setArray, steps, length, speed) {
   let left = array.slice(0, middle);
   let right = array.slice(middle);
   let leftArray = MergeSort(left, setArray, steps, length, speed);
+  steps.push(...leftArray);
   let rightArray = MergeSort(right, setArray, steps, length, speed);
-  let newArray = merge(leftArray, rightArray, setArray);
-  console.log([...newArray]);
+  steps.push(...rightArray);
+  let newArray = merge(array, leftArray, rightArray, setArray);
+  steps.push(...newArray);
+  console.log([...steps], "hello");
   if (newArray.length === length) {
-    setArray([...newArray]);
+    if (newArray === array) {
+      return;
+    }
+    let iterator = 1;
+    while (steps.length > 0) {
+      let activeBars = steps.shift();
+      let min = activeBars[0].id;
+      for (let bar of activeBars) {
+        if (bar.id < min) {
+          min = bar.id;
+        }
+        setTimeout(() => {
+          //console.log(bar);
+          //console.log(document.getElementsByClassName("dataBar")[bar.id]);
+          document
+            .getElementsByClassName("dataBar")
+            [bar.id].classList.add("active");
+          setTimeout(() => {
+            document
+              .getElementsByClassName("dataBar")
+              [bar.id].classList.remove("active");
+          }, speed);
+        }, speed * iterator);
+      }
+      setTimeout(() => {
+        array.splice(min, activeBars.length, ...activeBars);
+        setArray([...array]);
+      }, speed * iterator);
+      iterator++;
+    }
+    return array;
   }
-  // steps.push(newArray);
-  // if (steps[steps.length - 1].length === length) {
-  //   if (newArray === array) {
-  //     return;
-  //   }
-  //   let iterator = 1;
-  //   while (steps.length > 0) {
-  //     let activeBars = steps.shift();
-  //     let min = activeBars[0].id;
-  //     let max = activeBars[0].id;
-  //     for (let bar of activeBars) {
-  //       if (bar.id < min) {
-  //         min = bar.id;
-  //       } else if (bar.id > max) {
-  //         max = bar.id;
-  //       }
-  //       setTimeout(() => {
-  //         console.log(bar.id);
-  //         console.log(document.getElementsByClassName("dataBar")[bar.id]);
-  //         document.getElementsByClassName("dataBar")[bar.id].classList.add("active");
-  //         // document
-  //         //   .querySelector('li[data-id="' + bar.id + '"]')
-  //         //   .classList.add("active");
-  //         setTimeout(() => {
-  //           document.getElementsByClassName("dataBar")[bar.id].classList.remove("active");
-  //           // document
-  //           //   .querySelector('li[data-id="' + bar.id + '"]')
-  //           //   .classList.remove("active");
-  //         }, speed);
-  //       }, speed * iterator);
-  //     }
-  //     setTimeout(() => {
-  //       array.splice(min, activeBars.length, ...activeBars);
-  //       setArray([...array]);
-  //     }, speed * iterator);
-  //     iterator++;
-  //   }
-  // return array;
-  // }
   return newArray;
 }
 
