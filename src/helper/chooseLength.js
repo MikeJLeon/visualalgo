592e55f24b6eb050a07f2bf8f0ae61a1316b3530
Mike Leon <michaeljleon@live.com>
2020-11-13 14:56:08 -0800

    Removed unnessary files.

--- src/helper/chooseLength.js
+++ src/helper/chooseLength.js
@@ -1,6 +1,6 @@
 function chooseLength(length, setLength){
     if(length > 100){
-        length = 100;
+        //length = 100;
         document.getElementById("length").value = "100";
     }
     setLength(length);
